cd2857c58ddd066ef37bc027a852d67c83aa3104
kophonegap <niconi@coca-cola.com>
2016-07-29 17:46:20 +0800

    new

--- www/js/ui.js
+++ www/js/ui.js
@@ -511,7 +511,7 @@ appUI.populateCountryDetails = function(countryCode) {
 
 			//var imgPath = config.fileSystemRootFolder + '/' + config.localImageFolderPath + '/' + countryCode.toLowerCase() + ".png?" + Math.random();
 			
-			var imgPath = config.fileSystemRootFolder + '/' + config.localImageFolderPath2 + '/' + countryCode.toLowerCase() + ".png?" + Math.random();
+			var imgPath =  config.localImageFolderPath2 + '/' + countryCode.toLowerCase() + ".png?" + Math.random();
 		
 			//var iconPath =  config.localImageFolderPath2 + '/' + code.toLowerCase() + ".png?" + Math.random();
 			
